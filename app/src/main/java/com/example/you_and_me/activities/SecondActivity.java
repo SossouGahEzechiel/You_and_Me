babbbb85ddcea9e16a60d55f057a302ebcc84dd8
Dorcas6 <102770965+Dorcas6@users.noreply.github.com>
2022-05-02 19:18:09 +0000

    Squashed commit of the following:
    commit 6a403c5d7f367d8ca7f7cb9bb90e7b6d1e25ef7c
    Author: SossouGahEzechiel <ezecsossougah@gmail.com>
    Date:   Mon May 2 19:03:47 2022 +0000
    
        sauvetage
    
    commit 45bd2bbe7be085341d4f8065a43b9137b817248a
    Author: SossouGahEzechiel <ezecsossougah@gmail.com>
    Date:   Mon May 2 13:41:37 2022 +0000
    
        Passation de la donnée vers l'autre activité
    
    commit 317173694859f9248a54fe02b90d1845ae4ddd5f
    Author: Dorcas6 <102770965+Dorcas6@users.noreply.github.com>
    Date:   Mon May 2 11:15:06 2022 +0000
    
        Squashed commit of the following:
    
        commit fa6a20bd427ffe67c9c6797a7840b8aa19be4dc9
        Author: Dorcas6 <102770965+Dorcas6@users.noreply.github.com>
        Date:   Mon May 2 11:05:05 2022 +0000
    
            Envoi de la donnée vers la deuxième activité
    
        commit 37965f54ebf9f093013c8d273710809dc84d4294
        Author: Dorcas6 <102770965+Dorcas6@users.noreply.github.com>
        Date:   Mon May 2 10:31:39 2022 +0000
    
            Squashed commit of the following:
    
            commit aff54245df80d523eaae2200132d48be691062ab
            Author: SossouGahEzechiel <ezecsossougah@gmail.com>
            Date:   Mon May 2 00:37:21 2022 +0000
    
                Mise au point du code
    
            commit c81bc497a52e4f04b84171a4d62e1079e6e3190f
            Merge: 47c8297 b6cb91f
            Author: SossouGahEzechiel <ezecsossougah@gmail.com>
            Date:   Sun May 1 19:43:24 2022 +0000
    
                Merge branch 'master' into my_branch
    
            commit 47c82974e1daab892d96a80cea8b4a2044c54cc1
            Author: SossouGahEzechiel <ezecsossougah@gmail.com>
            Date:   Sun May 1 19:42:50 2022 +0000
    
                before merge
    
            commit b6cb91f39c7e88265d5e101cbce9fe205b0dab11
            Merge: dffe786 b79d7c8
            Author: Dorcas6 <102770965+Dorcas6@users.noreply.github.com>
            Date:   Sun May 1 19:33:51 2022 +0000
    
                Merge pull request #2 from SossouGahEzechiel/dev_branch
    
                back_end
    
            commit dffe78636a15c1caebe3527d6551f50b1e48d637
            Merge: 1386c02 379cbd5
            Author: Dorcas6 <102770965+Dorcas6@users.noreply.github.com>
            Date:   Sun May 1 17:02:10 2022 +0000
    
                Merge pull request #1 from SossouGahEzechiel/dev_branch
    
                Création du second fichier xml
    
        commit fd398b3cf3126f5715d822bcd6b013f5e2ace261
        Author: Dorcas6 <102770965+Dorcas6@users.noreply.github.com>
        Date:   Mon May 2 10:29:15 2022 +0000
    
            suite et tentative de correction du conflit
    
    commit aff54245df80d523eaae2200132d48be691062ab
    Author: SossouGahEzechiel <ezecsossougah@gmail.com>
    Date:   Mon May 2 00:37:21 2022 +0000
    
        Mise au point du code
    
    commit c81bc497a52e4f04b84171a4d62e1079e6e3190f
    Merge: 47c8297 b6cb91f
    Author: SossouGahEzechiel <ezecsossougah@gmail.com>
    Date:   Sun May 1 19:43:24 2022 +0000
    
        Merge branch 'master' into my_branch
    
    commit 47c82974e1daab892d96a80cea8b4a2044c54cc1
    Author: SossouGahEzechiel <ezecsossougah@gmail.com>
    Date:   Sun May 1 19:42:50 2022 +0000
    
        before merge
    
    commit b6cb91f39c7e88265d5e101cbce9fe205b0dab11
    Merge: dffe786 b79d7c8
    Author: Dorcas6 <102770965+Dorcas6@users.noreply.github.com>
    Date:   Sun May 1 19:33:51 2022 +0000
    
        Merge pull request #2 from SossouGahEzechiel/dev_branch
    
        back_end
    
    commit dffe78636a15c1caebe3527d6551f50b1e48d637
    Merge: 1386c02 379cbd5
    Author: Dorcas6 <102770965+Dorcas6@users.noreply.github.com>
    Date:   Sun May 1 17:02:10 2022 +0000
    
        Merge pull request #1 from SossouGahEzechiel/dev_branch
    
        Création du second fichier xml

--- app/src/main/java/com/example/you_and_me/activities/SecondActivity.java
+++ app/src/main/java/com/example/you_and_me/activities/SecondActivity.java
@@ -4,21 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.example.you_and_me.R;
+import com.example.you_and_me.entities.Personne;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TableLayout tableLayout;
+    Personne personne;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+        initializer();
+        personne = getIntentContent();
+        Log.i("contenu de personne",personne.toString());
+    }
 
-        Intent intent = getIntent();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        viewContentSetter();
+    }
 
+    private void initializer(){
+        tableLayout = findViewById(R.id.data_table);
+    }
 
+    private Personne getIntentContent(){
+        Bundle myBundle = getIntent().getExtras();
+        return myBundle.getParcelable("com.example.you_and_me.entities.Personne");
+    }
 
-
+    private void viewContentSetter(){
+        
     }
 }

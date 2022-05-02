6a403c5d7f367d8ca7f7cb9bb90e7b6d1e25ef7c
SossouGahEzechiel <ezecsossougah@gmail.com>
2022-05-02 19:03:47 +0000

    sauvetage

--- app/src/main/java/com/example/you_and_me/activities/SecondActivity.java
+++ app/src/main/java/com/example/you_and_me/activities/SecondActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.example.you_and_me.R;
@@ -12,18 +13,35 @@ import com.example.you_and_me.entities.Personne;
 
 public class SecondActivity extends AppCompatActivity {
 
+    TableLayout tableLayout;
     Personne personne;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+        initializer();
         personne = getIntentContent();
         Log.i("contenu de personne",personne.toString());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        viewContentSetter();
+    }
+
+    private void initializer(){
+        tableLayout = findViewById(R.id.data_table);
     }
 
     private Personne getIntentContent(){
         Bundle myBundle = getIntent().getExtras();
         return myBundle.getParcelable("com.example.you_and_me.entities.Personne");
+    }
+
+    private void viewContentSetter(){
+        
     }
 }
